d17ac67b6d25e484aa960ea90f4aa4abb2acdf88
Kyryll1304 <kirilltrosnickii@me.com>
2023-01-07 02:27:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+//Task one
+const arr = [
+  16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
+  76, -4, 12, -35, 4, 47,
+];
+
+let arrFiltered = [];
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; ++i) {
+  if (arr[i] > 0) {
+    sum += arr[i];
+  }
+}
+console.log(sum);
+
+//Task 1.5
+
+for (let i = 0; i < arr.length; ++i) {
+  if (arr[i] > 0) {
+    arrFiltered.push(arr[i]);
+  }
+}
+console.log(arrFiltered.length);
+
+//Task two
+
+const smallest = (element) => element === Math.min(...arr);
+
+console.log(arr.findIndex(smallest));
+
+//Task three
+const largest = (element) => element === Math.max(...arr);
+
+console.log(arr.findIndex(largest));
+//Task 4
+let arrMin = [];
+for (let i = 0; i < arr.length; ++i) {
+  if (arr[i] < 0) {
+    arrMin.push(arr[i]);
+  }
+}
+console.log(arrMin.length);
+//Task 5
+
+let arrEven = [];
+for (let i = 0; i < arr.length; ++i) {
+  if (arr[i] < 0) {
+    continue;
+  } else if (arr[i] % 2 != 0) {
+    arrEven.push(arr[i]);
+  }
+}
+console.log(arrEven);
+
+//Task 6
+
+let arrOdd = [];
+for (let i = 0; i < arr.length; ++i) {
+  if (arr[i] < 0) {
+    continue;
+  } else if (arr[i] % -2 == 0) {
+    arrOdd.push(arr[i]);
+  }
+}
+console.log(arrOdd);
